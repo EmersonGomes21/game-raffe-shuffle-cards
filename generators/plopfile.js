56af18c76c2ce0edd6508ad0ffa14f77e3023330
EmersonGomes21 <emerson.cursos.ti@gmail.com>
2022-08-05 14:36:11 -0300

    feat: refactor templates of plop

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -15,11 +15,11 @@ module.exports = function (plop) {
         path: '../src/components/{{pascalCase name}}/index.tsx',
         templateFile: 'templates/index.tsx.hbs'
       },
-      {
-        type: 'add',
-        path: '../src/components/{{pascalCase name}}/stories.tsx',
-        templateFile: 'templates/stories.tsx.hbs'
-      },
+      // {
+      //   type: 'add',
+      //   path: '../src/components/{{pascalCase name}}/stories.tsx',
+      //   templateFile: 'templates/stories.tsx.hbs'
+      // },
       {
         type: 'add',
         path: '../src/components/{{pascalCase name}}/styles.ts',
@@ -27,8 +27,8 @@ module.exports = function (plop) {
       },
       {
         type: 'add',
-        path: '../src/components/{{pascalCase name}}/test.tsx',
-        templateFile: 'templates/test.tsx.hbs'
+        path: '../src/components/{{pascalCase name}}/index.test.tsx',
+        templateFile: 'templates/index.test.tsx.hbs'
       }
     ]
   })
