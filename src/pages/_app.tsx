24abbfa4d90ddada42194d20b9ab265419ae2f91
EmersonGomes21 <emerson.cursos.ti@gmail.com>
2022-08-07 23:43:36 -0300

    feat: title and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,6 @@ function App({ Component, pageProps }: AppProps) {
         <link rel="apple-touch-icon" sizes="57x57" href="https://deckofcardsapi.com/static/img/favicon/apple-touch-icon-57x57.png" />
         <link rel="icon" type="image/png" href="https://deckofcardsapi.com/static/img/favicon/favicon-16x16.png" sizes="16x16" />
         <link rel="shortcut icon" href="https://deckofcardsapi.com/static/img/favicon/favicon.ico" />
-        <link rel="manifest" href="manifest.json" />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
