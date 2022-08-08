e874d9c748c5d6b57bb24cef707e9f1de0a50048
EmersonGomes21 <emerson.cursos.ti@gmail.com>
2022-08-07 23:44:44 -0300

    feat: context and hook useData

---
+++ src/hooks/context/index.tsx
@@ -0,0 +1,41 @@
+import React, { createContext, useCallback, useEffect, useState } from "react";
+import { apiCards,  } from "services/api";
+import { ICard, IContext } from "types";
+import { randomValue } from "utils";
+
+export const UserContext = createContext({} as IContext);
+
+export const ProviderDataPlayer: React.FC = ({ children }) => {
+    const [playerName, setPlayerName] = useState('');
+    const [cards, setDataCards] = useState<ICard[]>([]);
+    const [addNextCard, setAddNextCard] = useState(5);
+    const [countCards] = useState(8);
+
+    useEffect(() => {
+        if (cards?.length > 0) return
+        apiCards.getCards(countCards).then((result) => {
+            const cardsWithPoint = result?.cards?.map((card) => {
+                return {
+                    ...card,
+                    point: randomValue()
+                }
+            })  || []
+            setDataCards(cardsWithPoint)
+        })
+    }, [cards])
+
+    return (
+        <UserContext.Provider value={{
+            playerName,
+            setPlayerName,
+            cards,
+            addNextCard,
+            setAddNextCard,
+            setDataCards,
+            countCards
+        }}>
+            {children}
+        </UserContext.Provider>
+    );
+}
+
