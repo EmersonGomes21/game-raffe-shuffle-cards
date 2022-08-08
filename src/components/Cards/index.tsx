3f97081b2fbcd39ee9338f4349cec3e097625e2f
EmersonGomes21 <emerson.cursos.ti@gmail.com>
2022-08-08 10:53:11 -0300

    fix: Add LoaderAnimeted with lottie

--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -1,17 +1,30 @@
 import { CardItem } from 'components/CardItem'
 import { FooterButtons } from 'components/FooterButtons'
+import { LoaderAnimated } from 'components/LoaderAnimated'
 import { useData } from 'hooks/useData'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import * as S from './styles'
 
 export const Cards = () => {
   const { cards } = useData()
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const delay = 2000
+    const timeAuout = setTimeout(() => {
+      setLoading(false)
+    }, delay)
+
+    return () => clearTimeout(timeAuout)
+  }, [])
+  
   return (
     <S.Wrapper>
       {
-        cards?.length > 0 && cards.map((card) => <CardItem card={card} key={card.code} />
-        )
-         
+        loading ? <LoaderAnimated /> :
+          cards?.length > 0 && cards.map((card) => <CardItem card={card} key={card.code} />
+          )
+
       }
     </S.Wrapper>
   )
