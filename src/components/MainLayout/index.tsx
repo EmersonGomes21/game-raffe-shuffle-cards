ff8c7d9608ac8096884c9c157c0f4fdd854f824a
EmersonGomes21 <emerson.cursos.ti@gmail.com>
2022-08-07 23:27:10 -0300

    feat: Refactor MainLayout

---
+++ src/components/MainLayout/index.tsx
@@ -0,0 +1,32 @@
+import { AddPlayer } from 'components/AddPlayer'
+import { Cards } from 'components/Cards'
+import { FooterButtons } from 'components/FooterButtons'
+import { Header } from 'components/Header'
+import { useData } from 'hooks/useData'
+import React from 'react'
+
+import * as S from './styles'
+
+const MainLayout = ({
+
+}) => {
+  const { playerName } = useData()
+  return (
+    <S.Wrapper>
+      <Header />
+
+      {!playerName
+        ? <AddPlayer />
+        : (
+          <>
+            <Cards />
+            <FooterButtons />
+          </>
+        )
+      }
+
+    </S.Wrapper>
+  )
+}
+
+export { MainLayout }
